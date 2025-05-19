4274a6e14bc7bfa82e5edf9414231c0b22cb1d7c
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-05-19 13:09:32 -0300

    implantação do Docker

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `vio_rhuan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `vio_rhuan`;
+-- CREATE DATABASE  IF NOT EXISTS `vio_rhuan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- USE `vio_rhuan`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: vio_rhuan
