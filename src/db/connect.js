2c8868e651b0b55da35ea88c655cbf6a6c237613
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-09-29 13:18:40 -0300

    connect atualizado para railway

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST || process.env.MYSQLHOST,
+    user: process.env.DB_USER || process.env.MYSQLUSER,
+    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306
 })
 
 module.exports = pool;
