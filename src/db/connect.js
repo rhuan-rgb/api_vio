b51089cc6f3007ebaca4a7964bf48baeb360be7b
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-09-29 13:55:26 -0300

    inverti o connect

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,12 +2,13 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    connectionLimit:10,
-    host: process.env.DB_HOST || process.env.MYSQLHOST,
-    user: process.env.DB_USER || process.env.MYSQLUSER,
-    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
-    database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+    connectionLimit: 10,
+    host: process.env.MYSQLHOST || process.env.DB_HOST,
+    user: process.env.MYSQLUSER || process.env.DB_USER,
+    password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+    database: process.env.MYSQLDATABASE || process.env.DB_NAME,
     port: process.env.MYSQLPORT || 3306
 })
+
 
 module.exports = pool;
